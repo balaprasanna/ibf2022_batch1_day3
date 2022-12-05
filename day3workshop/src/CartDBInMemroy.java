f67515ce63c5249dc34a34b3824e4f2a85c65477
balaprasanna <balaprasannav2009@gmail.com>
2022-12-05 14:15:50 +0800

    Until List/Add/Login functionality

---
+++ day3workshop/src/CartDBInMemroy.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class CartDBInMemroy {
+    
+    public HashMap<String, ArrayList<String>> userMap = new HashMap<String, ArrayList<String>>();
+
+    public CartDBInMemroy() {
+    }
+
+    public void loadDataFromFiles() {
+        
+    }
+}
--- day3workshop/src/ShoppingCartDB.java
+++ day3workshop/src/ShoppingCartDB.java
@@ -1,12 +1,26 @@
-import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class ShoppingCartDB {
 
+    public static final String LOGIN = "login";
+    public static final String ADD = "add";
+    public static final String LIST = "list";
+    public static final String SAVE = "save";
+    public static final String EXIT = "exit";
+    public static final String USERS = "users";
+
     public static final List<String> VALID_COMMANDS = Arrays.asList(
-        "login", "add", "list", "exit", "users");
+        LOGIN, SAVE, ADD, LIST, USERS, EXIT);
+    
+    private CartDBInMemroy db;
+    private String currentUser;
+
+    public ShoppingCartDB() {
+        this.db = new CartDBInMemroy();
+    }
 
     public void startShell() {
         System.out.println("Welcome to MultiUser Shopping Cart >> ");
@@ -27,6 +41,7 @@ public class ShoppingCartDB {
                 System.out.println("Invalid Input: ^^");
             } else {
                 System.out.println("Processing : " + line);
+                this.ProcessInput(line);
             }
             
         }
@@ -40,5 +55,57 @@ public class ShoppingCartDB {
         return VALID_COMMANDS.contains(command);
     }
 
+    // Process command
+    public void ProcessInput(String input) {
+        Scanner sc  = new Scanner(input);
+        String command = sc.next().trim();
+
+        switch (command) {
+            case LOGIN:
+                String username = sc.nextLine().trim();
+                this.LoginAction(username);
+                System.out.println("Print - current logged in user" + this.currentUser);
+                break;
+
+            case LIST:
+                this.ListAction();
+                break;
+
+            case ADD:
+                String[] items = sc.nextLine().trim().split(",");
+                this.AddAction(items);
+                break;
+
+            case SAVE:
+                break;
+
+            case USERS:
+                break;
+
+            default:
+                break;
+        }
+
+        sc.close();
+    }
+
+    public void LoginAction(String username) {
+        if (!this.db.userMap.containsKey(username)) {
+            this.db.userMap.put(username, new ArrayList<String>());
+        }
+        this.currentUser = username;
+    }
+
+    public void AddAction(String[] items) {
+        for (String item : items) {
+            this.db.userMap.get(this.currentUser).add(item.trim());
+        }
+    }
+
+    public void ListAction() {
+        for (String item : this.db.userMap.get(this.currentUser)) {
+            System.out.println("Item -> " + item);
+        }
+    }
 
 }
